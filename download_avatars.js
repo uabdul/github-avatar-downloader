8a5a1342e5c997ed8cfc437eea74f38187887010
Umair Abdul Qadir <umair.abdulq@gmail.com>
2018-12-03 22:12:21 +0000

    Modifying download_avatars.js to print each avatar_url

--- download_avatars.js
+++ download_avatars.js
@@ -1,23 +1,27 @@
 var request = require('request');
+var token = require('./secrets.js')
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors"
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
-      'User-Agent': 'request'
-      'Authorization': ''
+      'User-Agent': 'request',
+      'Authorization': token['GITHUB_TOKEN']
     }
   };
 
   request(options, function(err, res, body){
-    cb(err, body);
+    var arr = JSON.parse(body);
+    cb(err, arr);
   });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  result.forEach(function (object) {
+    console.log(object['avatar_url']);
+  })
 });
 
