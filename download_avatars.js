7c7a8e390a420cb6c129aa40a3fad41fb7690bec
Umair Abdul Qadir <umair.abdulq@gmail.com>
2018-12-04 00:25:58 +0000

    To modifying code to support command line arguments, make an /avatars directory if one doesn't exist, and make the repo owner and name required arguments.

--- download_avatars.js
+++ download_avatars.js
@@ -1,38 +1,49 @@
 var request = require('request');
 var token = require('./secrets.js');
 var fs = require('fs');
+var repoOwner = process.argv[2];
+var repoName = process.argv[3];
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-    headers: {
-      'User-Agent': 'request',
-      'Authorization': token['GITHUB_TOKEN']
-    }
-  };
+  if (!repoName || !repoOwner) {
+    console.log("Please provide a valid repo owner and repo name. Your inputs should be in the following format: node download_avatars.js <repo owner> <repo name>")
+    return;
+  } else {
+    var options = {
+      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+      headers: {
+        'User-Agent': 'request',
+        'Authorization': "token " + token['GITHUB_TOKEN']
+      }
+    };
 
-  request(options, function(err, res, body){
-    var arr = JSON.parse(body);
-    cb(err, arr);
-  });
+    request(options, function(err, res, body){
+      var arr = JSON.parse(body);
+      parseData(err, arr);
+    });
+  }
 }
 
 function downloadImageByURL(url, filePath) {
-  var finalFilePath = "./avatars/" + filePath
+  if (!fs.existsSync("./avatars/")) {
+    fs.mkdirSync("./avatars/");
+  }
+  var finalFilePath = "./avatars/" + filePath + ".png"
   request.get(url).pipe(fs.createWriteStream(finalFilePath));
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+
+function parseData(err, result) {
   console.log("Errors:", err);
   result.forEach(function (object) {
     var url = object['avatar_url'];
     var filePath = object['login'];
     downloadImageByURL(url, filePath);
-  })
-});
+  });
+  console.log("The avatars have been downloaded! Please check the /avatars folder.")
+}
 
-// downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
+getRepoContributors(repoOwner, repoName);
 
-// request.get('http://google.com/img.png').pipe(request.put('http://mysite.com/img.png'))
