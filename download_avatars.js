f5afbd38a19f612b19e2954989ff8e6e7b466ed4
Umair Abdul Qadir <umair.abdulq@gmail.com>
2018-12-04 00:39:29 +0000

    To final comments.

--- download_avatars.js
+++ download_avatars.js
@@ -6,6 +6,9 @@ var repoName = process.argv[3];
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+// If a valid repo owner and name not provided, this function prompts the user
+//for correct input and terminates the application. If correct inputs provided,
+//the function submits the request and parses the result into an array.
 function getRepoContributors(repoOwner, repoName, cb) {
   if (!repoName || !repoOwner) {
     console.log("Please provide a valid repo owner and repo name. Your inputs should be in the following format: node download_avatars.js <repo owner> <repo name>")
@@ -26,6 +29,8 @@ function getRepoContributors(repoOwner, repoName, cb) {
   }
 }
 
+//If a folder named /avatars/ does not exist, creates a folder.
+//For each user, requests and writes display image.
 function downloadImageByURL(url, filePath) {
   if (!fs.existsSync("./avatars/")) {
     fs.mkdirSync("./avatars/");
@@ -35,6 +40,8 @@ function downloadImageByURL(url, filePath) {
 }
 
 
+//Iterates through each object in the array and identifies the URL and filepath,
+//and calls back the downloadImagesByURL function.
 function parseData(err, result) {
   console.log("Errors:", err);
   result.forEach(function (object) {
@@ -44,6 +51,8 @@ function parseData(err, result) {
   });
   console.log("The avatars have been downloaded! Please check the /avatars folder.")
 }
+
+//Initiates the program using inputs provided by the Node user.
 
 getRepoContributors(repoOwner, repoName);
 
